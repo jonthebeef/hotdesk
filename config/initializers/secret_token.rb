5e3b001a7abb8176b8f2f6f7fdaaceb8073d79e2
jon grant <jon@alfieandjon.com>
2013-03-09 16:59:15 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotdesk::Application.config.secret_token = 'dd37178dfc0ba4b7216101e6a94e296f406dbbacd8b2a322d621e338618317b740be7daf9f0be65303e7fc316d4e1fa123152267c1615de0d56c6d3c2dee1785'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Hotdesk::Application.config.session_store :cookie_store, key: '_hotdesk_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Hotdesk::Application.config.session_store :active_record_store
